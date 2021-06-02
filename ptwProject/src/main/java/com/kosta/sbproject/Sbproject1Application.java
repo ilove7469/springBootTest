c94c4e7fe3b8cf056a254f9397d8a269d9ab0d18
ptw <mylove7469@naver.com>
2021-06-02 16:57:44 +0900

    STS 깃허브테스트

---
+++ ptwProject/src/main/java/com/kosta/sbproject/Sbproject1Application.java
@@ -0,0 +1,13 @@
+package com.kosta.sbproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sbproject1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sbproject1Application.class, args);
+	}
+
+}
